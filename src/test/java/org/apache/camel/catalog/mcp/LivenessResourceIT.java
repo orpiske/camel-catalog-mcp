7343035a8cced2e9e5cc709f58e5118c69903a57
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-04-30 17:56:50 +0200

    Delete unused tests

--- src/test/java/org/apache/camel/catalog/mcp/LivenessResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.apache.camel.catalog.mcp;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class LivenessResourceIT extends LivenessResourceTest {
-    // Execute the same tests but in packaged mode.
-}
