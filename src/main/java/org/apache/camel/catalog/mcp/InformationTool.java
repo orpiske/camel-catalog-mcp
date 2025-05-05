560a73c1731ced4654761fe9b3e9b53f04a138f4
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-05-05 10:51:49 +0200

    Added support for endpoint options

--- src/main/java/org/apache/camel/catalog/mcp/InformationTool.java
+++ src/main/java/org/apache/camel/catalog/mcp/InformationTool.java
@@ -72,4 +72,49 @@ public class InformationTool {
 
         return ToolResponse.success(reply.toString());
     }
+
+
+    @Tool(description = "Provide information about the endpoint options provided by an Apache Camel component")
+    public ToolResponse getInformationAboutQueryParameterOptions(@ToolArg(description = "The name of the component to get information for") String componentName) {
+        final ComponentModel componentModel = catalog.componentModel(componentName.toLowerCase());
+
+        JsonObject reply = new JsonObject();
+        JsonArray array = new JsonArray();
+        final List<ComponentModel.EndpointOptionModel> componentOptions = componentModel.getEndpointOptions();
+        for (var option : componentOptions) {
+            JsonObject optionObj = new JsonObject();
+            optionObj.put("name", option.getName());
+            optionObj.put("kind", option.getKind());
+            optionObj.put("description", option.getDescription());
+
+            array.add(optionObj);
+        }
+
+        reply.put("options", array);
+
+        return ToolResponse.success(reply.toString());
+    }
+
+    @Tool(description = "Provide information about an specific configuration option provided by an Apache Camel endpoint option")
+    public ToolResponse getInformationAboutSpecificEndpointOptions(
+            @ToolArg(description = "The name of the component to get information for") String componentName,
+            @ToolArg(description = "The name of the option to get information for") String optionName) {
+        final String s = catalog.listComponentsAsJson();
+        final ComponentModel componentModel = catalog.componentModel(componentName.toLowerCase());
+
+        JsonObject reply = new JsonObject();
+        final List<ComponentModel.EndpointOptionModel> componentOptions = componentModel.getEndpointOptions();
+        final ComponentModel.EndpointOptionModel option =
+                componentOptions.stream().filter(c -> c.getName().equals(optionName)).findFirst().get();
+
+        reply.put("name", option.getName());
+        reply.put("description", option.getDescription());
+        reply.put("kind", option.getKind());
+        reply.put("type", option.getType());
+        reply.put("defaultValue", option.getDefaultValue());
+
+        return ToolResponse.success(reply.toString());
+    }
+
+
 }
